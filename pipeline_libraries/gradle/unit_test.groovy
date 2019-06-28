7dc5ccb0b0e8ccf13ea21a5c44633c3be2708861
ottoson-keegan <ottoson_keegan@bah.com>
2019-06-27 22:09:07 -0400

    testing pipeline-libraries unit testing step

--- pipeline_libraries/gradle/unit_test.groovy
+++ pipeline_libraries/gradle/unit_test.groovy
@@ -3,7 +3,7 @@ def call() {
     unstash "workspace"
     // if no gradle image set, use latest
     sh "docker build -f unit_test.Dockerfile -t pipeline-unit-testing ."
-    sh "docker run --rm -t -v \$(pwd):/app -w /app pipeline-unit-testing gradle --no-daemon test"
+    sh "docker run --rm -t -v \$(pwd):/app -w /app --oom-kill-disable -m 3500m pipeline-unit-testing gradle --no-daemon test"
     sh "ls -al"
   }
     //archiveArtifacts artifacts: 'target/reports/tests/test/**'
