fa335a7914ccaf91af64e88b7e58463f5a74f1a6
ottoson-keegan <ottoson_keegan@bah.com>
2019-06-27 21:17:02 -0400

    fixing typo

--- pipeline_libraries/gradle/unit_test.groovy
+++ pipeline_libraries/gradle/unit_test.groovy
@@ -4,6 +4,6 @@ def call() {
     // if no gradle image set, use latest
     sh "docker build -f unit_test.Dockerfile -t pipeline-unit-testing ."
     sh "docker run --rm -t -v \$(shell pwd):/app -w /app pipeline-unit-testing gradle --no-daemon test"
-
+  }
     //archiveArtifacts artifacts: 'target/reports/tests/test/**'
 }
