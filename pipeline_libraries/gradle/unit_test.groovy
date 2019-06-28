36503b2eaa1da90bc5edfb39128a5d4cd3bcbcd0
ottoson-keegan <ottoson_keegan@bah.com>
2019-06-28 12:58:34 -0400

    testing pipeline-libraries unit testing step

--- pipeline_libraries/gradle/unit_test.groovy
+++ pipeline_libraries/gradle/unit_test.groovy
@@ -2,7 +2,7 @@ def call() {
   node {
     unstash "workspace"
     // if no gradle image set, use latest
-    docker.image(config.image.name).inside("-m 3000m"){
+    docker.image(config.image.name).inside("-m 3500m"){
       sh "gradle --no-daemon clean test "
     }
   }
