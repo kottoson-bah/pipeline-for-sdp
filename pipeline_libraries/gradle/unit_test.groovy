7a2f365db9bf893f3be5b24f4f64f6b6ecca4de0
ottoson-keegan <ottoson_keegan@bah.com>
2019-06-30 11:08:30 -0400

    testing pipeline-libraries unit testing step

--- pipeline_libraries/gradle/unit_test.groovy
+++ pipeline_libraries/gradle/unit_test.groovy
@@ -5,8 +5,10 @@ def call() {
     //   sh "./gradlew --no-daemon clean test "
     // }
 
-    sh "docker build -f unit_test.Dockerfile -t pipeline-unit-testing ."
-    sh "docker run --rm -t -v \$(pwd):/app -w /app pipeline-unit-testing gradle --no-daemon test"
+    // sh "docker build -f unit_test.Dockerfile -t pipeline-unit-testing ."
+    // sh "docker run --rm -t -v \$(pwd):/app -w /app pipeline-unit-testing gradle --no-daemon test"
+
+    sh "./gradlew test"
     sh "ls -al"
 
   }
