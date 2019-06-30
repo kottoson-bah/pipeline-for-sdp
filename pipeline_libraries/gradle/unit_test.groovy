651cdd734cd1155a05f309752f4fe375e5da1149
ottoson-keegan <ottoson_keegan@bah.com>
2019-06-30 15:21:19 -0400

    updating unit test step

--- pipeline_libraries/gradle/unit_test.groovy
+++ pipeline_libraries/gradle/unit_test.groovy
@@ -1,16 +1,17 @@
 def call() {
-  node {
-    unstash "workspace"
-    // docker.image(config.image.name).inside("-m 4000m"){
-    //   sh "./gradlew --no-daemon clean test "
-    // }
+  stage("Unit Test"){
+    node {
+      unstash "workspace"
+      docker.image(config.image.name).inside("-m 4000m"){
+        sh "./gradlew --no-daemon clean test"
+        archiveArtifacts artifacts: 'target/reports/tests/test/**'
+      }
 
-    // sh "docker build -f unit_test.Dockerfile -t pipeline-unit-testing ."
-    // sh "docker run --rm -t -v \$(pwd):/app -w /app pipeline-unit-testing gradle --no-daemon test"
+      // sh "docker build -f unit_test.Dockerfile -t pipeline-unit-testing ."
+      // sh "docker run --rm -t -v \$(pwd):/app -w /app pipeline-unit-testing gradle --no-daemon test"
 
-    sh "./gradlew test"
-    sh "ls -al"
-
+      // sh "./gradlew test"
+      // sh "ls -al"
+    }
   }
-    //archiveArtifacts artifacts: 'target/reports/tests/test/**'
 }
